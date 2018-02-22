9bceb02dc62302ad3aba9fd23f779a02a77f0bf3
David Talan <dav3meiister@gmail.com>
2018-02-22 14:55:03 +0000

    ui update

--- medicalDatabase/medicalDatabase/settings.py
+++ medicalDatabase/medicalDatabase/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'medicalDatabase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'my_database',
     }
 }
 
