fcb258f6de824b500c4325d4b47c22fc8739fac6
David Talan <dav3meiister@gmail.com>
2018-03-01 00:47:21 +0000

    testing queries

--- code/Blockchain/query.py
+++ code/Blockchain/query.py
@@ -1,8 +1,10 @@
 import pymongo
+from bigchaindb_driver import BigchainDB
 from pymongo import MongoClient
 import pprint
 
 client = MongoClient('127.0.0.1:9984')
-db = client.test_database
-posts= db.digital_asset_payload
-pprint.pprint(posts.find_one({"api":"v1"}))
+root_url = 'http://127.0.0.1:9984/api/v1/transactions/32a184631b7cc4f2878540450f465d82c275f61ce5e4a3c22c840666987e1591'
+db = BigchainDB(root_url)
+assets= db.assets
+print(assets.get(search ='asset'))
