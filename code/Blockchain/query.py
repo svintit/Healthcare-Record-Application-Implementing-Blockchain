56158d4a5b82af37ea0947070551db43b70fa160
David Talan <dav3meiister@gmail.com>
2018-03-07 20:34:58 +0000

    http

--- code/Blockchain/query.py
+++ code/Blockchain/query.py
@@ -3,8 +3,13 @@ from bigchaindb_driver import BigchainDB
 from pymongo import MongoClient
 import pprint
 
-client = MongoClient('127.0.0.1:9984')
-root_url = 'http://127.0.0.1:9984/api/v1/transactions/32a184631b7cc4f2878540450f465d82c275f61ce5e4a3c22c840666987e1591'
-db = BigchainDB(root_url)
-assets= db.assets
-print(assets.get(search ='asset'))
+client = MongoClient('http://127.0.0.1:9984/')
+db = client.my_database
+collection = db.my_collection
+
+#http://api.mongodb.com/python/current/tutorial.html#tutorial
+
+#root_url = 'http://127.0.0.1:9984/api/v1/transactions/32a184631b7cc4f2878540450f465d82c275f61ce5e4a3c22c840666987e1591'
+#db = BigchainDB(root_url)
+#assets= db.assets
+#print(assets.get(search ='asset'))
