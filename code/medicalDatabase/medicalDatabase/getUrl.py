d0550f3318378c976b01bc419e85a9b0ae614903
David Talan <dav3meiister@gmail.com>
2018-03-08 12:42:07 +0000

    json

--- code/medicalDatabase/medicalDatabase/getUrl.py
+++ code/medicalDatabase/medicalDatabase/getUrl.py
@@ -1,5 +1,5 @@
 import requests
 
 class getUrl():
-	result = requests.get('http://www.google.com')
+	result = requests.get('http://127.0.0.1:9984/api/v1/transactions/d9971cb8074e6d3e7a71b95dbdace0466a6d8f9759e583d1911c619f55a62e18')
 	print(result.text)
